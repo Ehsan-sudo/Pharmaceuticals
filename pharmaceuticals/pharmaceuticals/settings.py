282c9fd98d065eb0aadd41559d26e2149da20b30
Ehsan Popal <popalehsan26@gmail.com>
2022-04-05 17:11:25 +0430

    login, logout, register added

--- pharmaceuticals/pharmaceuticals/settings.py
+++ pharmaceuticals/pharmaceuticals/settings.py
@@ -123,6 +123,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'home'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- pharmaceuticals/user/views.py
+++ pharmaceuticals/user/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login as auth_login, logout
 
 
 def register(request):
@@ -14,8 +14,26 @@ def register(request):
             print(username, password)
             user = User(username=username, password=password)
             user.save()
-            login(request, user)
-            messages.success(request, f'Welcome {username}!')
-            return redirect('home')
+            return login_user(request, user)
     else:
         return render(request, 'users/register.html')
+
+def login_user(request, user):
+    auth_login(request, user)
+    messages.success(request, f'Welcome {user.username}!')
+    return redirect('home')
+
+def login(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('userpassword')
+        print(username, password)
+        user = User.objects.filter(username=username, password=password).first()
+        print(user)
+        if user:
+            return login_user(request, user)
+        else:
+            messages.error(request, f'Username or password incorrect!')
+    return render(request, 'users/login.html')
+        
+
