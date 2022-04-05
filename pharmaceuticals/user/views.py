470c11a60a11ba9abaa7449715d14d82df3c2891
Ehsan Popal <popalehsan26@gmail.com>
2022-04-05 15:37:29 +0430

    registeration page created, automated login added

--- pharmaceuticals/user/views.py
+++ pharmaceuticals/user/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 
 def register(request):
-    return render(request, 'users/register.html')
+    if request.POST:
+        # if form is valid:
+        username = request.POST.get('username')
+        password = request.POST.get('userpassword')
+        confirm = request.POST.get('confirm')
+        if password == confirm:
+            print(username, password)
+            user = User(username=username, password=password)
+            user.save()
+            login(request, user)
+            messages.success(request, f'Welcome {username}!')
+            return redirect('home')
+    else:
+        return render(request, 'users/register.html')
