2291190bd623120cb1a44490ddb857a850e1b6be
Ehsan Popal <popalehsan26@gmail.com>
2022-04-07 15:44:23 +0430

    comapny module CRUD completed, search remaining

--- pharmaceuticals/medicine/urls.py
+++ pharmaceuticals/medicine/urls.py
@@ -4,5 +4,10 @@ from . import views
 urlpatterns = [
     path('home/', views.home, name='home'),
     path('list-company/', views.list_company, name='list_company'),
+    path('list-company/<int:id>/', views.get_company, name='get_company'),
     path('add-company/', views.add_company, name='add_company'),
+    path('delete-company/<int:id>/', views.delete_company, name='delete_company'),
+
+    path('not-found-404/', views.page_404, name='page_404'),
+
 ]
--- pharmaceuticals/medicine/views.py
+++ pharmaceuticals/medicine/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from medicine.models import Company
 
 # Create your views here.
 
@@ -14,7 +16,42 @@ def home(request):
 
 
 def list_company(request):
-    return render(request, 'medicine/company/list_company.html')
+    companies = Company.objects.all()
+    return render(request, 'medicine/company/list_company.html', {'companies':companies})
 
 def add_company(request):
+    if request.POST:
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        location = request.POST.get('address')
+        company = Company.objects.create(name=name, email=email, phone=phone, location=location)
+        return redirect('list_company')
     return render(request, 'medicine/company/add_company.html')
+
+def get_company(request, id):
+    if Company.objects.filter(id=id).exists():
+        company = Company.objects.get(pk=id)
+
+        if request.POST:
+            company.name = request.POST.get('name')
+            company.email = request.POST.get('email')
+            company.phone = request.POST.get('phone')
+            company.location = request.POST.get('location')
+            company.save()
+            return redirect('list_company')
+        else:
+            return render(request, 'medicine/company/get_company.html', {'company':company})
+    else:
+        return redirect('page_404')
+
+def delete_company(request, id):
+    if Company.objects.filter(id=id).exists():
+        Company.objects.get(pk=id).delete()
+        return redirect('list_company')
+    else:
+        return redirect('page_404')
+
+
+def page_404(request):
+    return render(request, 'medicine/utility/pages-404.html')
